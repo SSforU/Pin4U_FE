7e90888912396966cc9ee3824e504281286e546e
ukyeong-user <hyk040520@naver.com>
2025-08-17 21:20:54 +0900

    feat: 공통 컴포넌트(진행바) 생성

---
+++ src/component/ui/ProgressBar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+
+function ProgressBar({ currentStep = 1, totalSteps = 3, ...props }) {
+  return (
+    <div {...props}>
+      <ProgressBarContainer>
+        <ProgressFill currentStep={currentStep} totalSteps={totalSteps} />
+      </ProgressBarContainer>
+    </div>
+  );
+}
+
+export default ProgressBar;
+
+//styled-components
+const ProgressBarContainer = styled.div`
+  width: 330px;
+  height: 6px;
+  background-color: #d9d9d9;
+  border-radius: 3px;
+  overflow: hidden;
+  position: relative;
+`;
+
+const ProgressFill = styled.div`
+  height: 100%;
+  background-color: #ff7e74;
+  border-radius: 3px;
+  transition: width 0.5s ease-in-out;
+  width: ${(props) => {
+    const progress = (props.currentStep / props.totalSteps) * 100;
+    return `${progress}%`;
+  }};
+`;
