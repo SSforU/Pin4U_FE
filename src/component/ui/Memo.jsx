bac63cfc6b25439d80aa9c70c712c50ba29f24e2
ukyeong-user <hyk040520@naver.com>
2025-08-19 18:11:32 +0900

    feat: ui 폴더에 Memo 컴포넌트 추가

---
+++ src/component/ui/Memo.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledTextarea = styled.textarea`
+  width: 100%;
+  ${(props) => props.height && `height:${props.height}px;`}
+  padding: 16px 14px;
+  font-family: "Pretendard", sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 18px;
+  border: 1px solid #bababa;
+  border-radius: 10px;
+  background-color: #f7f7f7;
+  resize: none;
+  box-sizing: border-box;
+  overflow: hidden;
+
+  &::placeholder {
+    color: #bababa;
+  }
+
+  &:focus {
+    outline: none;
+    border-color: #ff7e74;
+    background-color: #ffffff;
+  }
+`;
+
+function Memo(props) {
+  const { height, value, onChange, placeholder, maxLength } = props;
+  return (
+    <StyledTextarea
+      height={height}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+      maxLength={maxLength}
+    />
+  );
+}
+
+export default Memo;
