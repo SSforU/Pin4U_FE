2e9b7dbc2496522511aa6efbbb064a9181baf4b1
ukyeong-user <hyk040520@naver.com>
2025-08-24 01:45:44 +0900

    feat: 최상단 컴포넌트 App.jsx에 #1 고정사용자 조회 api 연동

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,13 @@ import styled from "styled-components";
 function App() {
   const [userProfile, setUserProfile] = useState(null);
   const [loading, setLoading] = useState(true);
-
+  const BASE_URL = import.meta.env.VITE_BASE_URL;
   // 고정 사용자(id=1) 프로필 조회
   useEffect(() => {
     const fetchUserProfile = async () => {
       try {
         setLoading(true);
-        const response = await axios.get("/api/users/1");
+        const response = await axios.get(`${BASE_URL}/api/user/1`);
 
         if (response.data.result === "success") {
           setUserProfile(response.data.data.user);
