378a9437db9408a3f5fb11f0272648208d1985b9
ukyeong-user <hyk040520@naver.com>
2025-09-26 16:54:50 +0900

    fix/잘못된 경로 수정

--- src/utils/oauth.js
+++ src/utils/oauth.js
@@ -2,6 +2,10 @@
 const FRONTEND_URL =
   import.meta.env.VITE_KAKAO_REDIRECT_URI || "http://localhost:5173";
 export const CLIENT_ID = "d5e14b6afd14509b1aee4ef3de5b99b2"; // 실제 카카오 앱 키로 교체 필요
-export const REDIRECT_URI = `${FRONTEND_URL}/oauth/callback/kakao`;
+
+// 환경변수에 이미 전체 경로가 있는지 확인
+export const REDIRECT_URI = FRONTEND_URL.includes("/oauth/callback/kakao")
+  ? FRONTEND_URL
+  : `${FRONTEND_URL}/oauth/callback/kakao`;
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
